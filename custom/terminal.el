dd08469d78a03be47559dd2fead62998de9189ef
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-01-15 15:31:06 -0600

    Replace term/xterm by vterm.

--- custom/terminal.el
+++ custom/terminal.el
@@ -12,43 +12,58 @@
           (lambda() (setq yas-dont-activate t)
             (linum-mode 0)))
 
-(defadvice term-sentinel
-    (around my-advice-term-sentinel (proc msg))
-  ":v."
-  (if (memq (process-status proc) '(signal exit))
-      (let ((buffer (process-buffer proc)))
-        ad-do-it
-        (kill-buffer buffer))
-    ad-do-it))
-(ad-activate 'term-sentinel)
+;; (defadvice term-sentinel
+;;     (around my-advice-term-sentinel (proc msg))
+;;   ":v."
+;;   (if (memq (process-status proc) '(signal exit))
+;;       (let ((buffer (process-buffer proc)))
+;;         ad-do-it
+;;         (kill-buffer buffer))
+;;     ad-do-it))
+;; (ad-activate 'term-sentinel)
 
-(defvar my-term-shell "/bin/bash")
+;; (defvar my-term-shell "/bin/bash")
 
-(defadvice ansi-term (before force-bash)
-  ":v."
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
+;; (defadvice ansi-term (before force-bash)
+;;   ":v."
+;;   (interactive (list my-term-shell)))
+;; (ad-activate 'ansi-term)
 
-(defun my-term-use-utf8 ()
-  ":v."
-  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+;; (defun my-term-use-utf8 ()
+;;   ":v."
+;;   (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
 
-(add-hook 'term-exec-hook 'my-term-use-utf8)
+;; (add-hook 'term-exec-hook 'my-term-use-utf8)
 
-(defun my-term-paste (&optional string)
-  ":v STRING."
-  (interactive)
-  (process-send-string
-   (get-buffer-process (current-buffer))
-   (if string string (current-kill 0))))
+;; (defun my-term-paste (&optional string)
+;;   ":v STRING."
+;;   (interactive)
+;;   (process-send-string
+;;    (get-buffer-process (current-buffer))
+;;    (if string string (current-kill 0))))
 
-(defun my-term-hook ()
-  ":v."
-  (goto-address-mode)
-  (define-key term-raw-map "\C-y" 'my-term-paste)
-  (setq term-buffer-maximum-size 0))
+;; (defun my-term-hook ()
+;;   ":v."
+;;   (goto-address-mode)
+;;   (define-key term-raw-map "\C-y" 'my-term-paste)
+;;   (setq term-buffer-maximum-size 0))
 
-(add-hook 'term-mode-hook 'my-term-hook)
+;; (add-hook 'term-mode-hook 'my-term-hook)
+
+(use-package vterm
+  :ensure t
+  :custom
+  (vterm-shell "fish")
+  (vterm-copy-exclude-prompt t)
+  (vterm-buffer-name-string "*vterm %s")
+  :config
+  (add-hook 'vterm-mode-hook
+            (lambda ()
+              (set (make-local-variable 'buffer-face-mode-face) '(:family "MesloLGS NF" :weight bold :height 120))
+              (buffer-face-mode t)))
+  (define-key vterm-mode-map (kbd "C-m") #'vterm-send-return))
+
+
 
 (provide 'terminal)
 ;;; terminal.el ends here
