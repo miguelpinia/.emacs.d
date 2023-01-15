03214948e7ee3a330c771ef8f1a676ac22d3fa5e
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-01-15 15:30:38 -0600

    Add pyvenv for python environments evaluation.

--- custom/setup-py.el
+++ custom/setup-py.el
@@ -7,6 +7,7 @@
   :custom
   (python-shell-interpreter "/usr/bin/python3"))
 
+
 ;; (use-package elpy
 ;;   :ensure t
 ;;   :init (elpy-enable)
@@ -14,6 +15,24 @@
 ;;   (elpy-rpc-python-command "python3")
 ;;   (elpy-rpc-timeout 2)
 ;;   (elpy-shell-echo-output nil))
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
+(use-package pyvenv-auto
+  :ensure t
+  :init (pyvenv-auto-mode t)
+  :custom
+  (pyvenv-auto-venv-dirnames '("venv" ".venv" ".env")))
 
 ;; (use-package pyenv
 ;;   :ensure t
